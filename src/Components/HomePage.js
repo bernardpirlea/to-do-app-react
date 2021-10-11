95b443f0e5c517d03377d03320f05325396b734a
Bernard Pirlea <pirlea.bernard@gmail.com>
2021-10-11 12:31:19 +0300

    Created component for Register

--- src/Components/HomePage.js
+++ src/Components/HomePage.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./css/HomePage.css";
 import logoImage from "../Images/to-do-logo.png";
 import Login from "./Login";
+import Signup from "./Signup";
 
 const HomePage = () => {
   return (
@@ -10,6 +11,7 @@ const HomePage = () => {
         <img src={logoImage} alt="Logo Image" />
       </div>
       <Login />
+      {/* <Signup /> */}
     </div>
   );
 };
--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -1,10 +1,15 @@
 import React from "react";
-import { Link } from "react-router-dom";
 
-const Login = () => {
+const Signup = () => {
   return (
     <div className="login-section">
       <form className="col-5">
+        <div className="mb-3">
+          <label for="emailInput" class="form-label">
+            Email
+          </label>
+          <input type="email" class="form-control" id="emailInput" />
+        </div>
         <div className="mb-3">
           <label for="usernameInput" class="form-label">
             Username
@@ -18,11 +23,11 @@ const Login = () => {
           <input type="password" class="form-control" id="passwordInput" />
         </div>
         <button type="submit" class="btn login-button">
-          Login
+          Sign up
         </button>
       </form>
     </div>
   );
 };
 
-export default Login;
+export default Signup;
