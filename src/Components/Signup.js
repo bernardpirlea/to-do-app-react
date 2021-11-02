2d0072ef3a041244b3a814e45b0152594a2455cb
Bernard Pirlea <pirlea.bernard@gmail.com>
2021-11-02 21:39:43 +0200

    Error handling for sign up

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -10,6 +10,7 @@ const initialValues = {
 const Signup = ({ changeMode, setToken }) => {
   const [values, setValues] = useState(initialValues);
   const [errors, setErrors] = useState(initialValues);
+  const [submitError, setSubmitError] = useState();
 
   const validation = () => {
     let formIsValid = true;
@@ -47,7 +48,11 @@ const Signup = ({ changeMode, setToken }) => {
           "http://localhost:8000/api/register/",
           values
         );
-        setToken(response.data.token);
+        if (response.data.error) {
+          setSubmitError(response.data.error);
+        } else {
+          setToken(response.data.token);
+        }
       }
       return;
     } catch (error) {
@@ -56,60 +61,63 @@ const Signup = ({ changeMode, setToken }) => {
   };
 
   return (
-    <div className="login-section">
-      <form className="col-5">
-        <div className="mb-3">
-          <label for="emailInput" class="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="emailInput"
-            value={values.email}
-            onChange={handleInputChange}
-            name="email"
-          />
-          <span style={{ color: "red" }}>{errors["email"]}</span>
-        </div>
-        <div className="mb-3">
-          <label for="usernameInput" class="form-label">
-            Username
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="usernameInput"
-            value={values.username}
-            onChange={handleInputChange}
-            name="username"
-          />
-          <span style={{ color: "red" }}>{errors["username"]}</span>
-        </div>
-        <div className="mb-3">
-          <label for="passwordInput" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="passwordInput"
-            value={values.password}
-            onChange={handleInputChange}
-            name="password"
-          />
-          <span style={{ color: "red" }}>{errors["password"]}</span>
-        </div>
-        <button type="submit" class="btn login-button" onClick={handleSubmit}>
-          Sign up
-        </button>
-        <span className="mx-2">
-          Already a member?
-          <button className="link" onClick={handleClick}>
-            Log In!
+    <div className="login col-9 col-md-8 col-lg-6 col-xl-5">
+      <p style={{ color: "red" }}>{submitError}</p>
+      <div className="login-section">
+        <form>
+          <div className="mb-3">
+            <label for="emailInput" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="emailInput"
+              value={values.email}
+              onChange={handleInputChange}
+              name="email"
+            />
+            <span style={{ color: "red" }}>{errors["email"]}</span>
+          </div>
+          <div className="mb-3">
+            <label for="usernameInput" class="form-label">
+              Username
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="usernameInput"
+              value={values.username}
+              onChange={handleInputChange}
+              name="username"
+            />
+            <span style={{ color: "red" }}>{errors["username"]}</span>
+          </div>
+          <div className="mb-3">
+            <label for="passwordInput" class="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="passwordInput"
+              value={values.password}
+              onChange={handleInputChange}
+              name="password"
+            />
+            <span style={{ color: "red" }}>{errors["password"]}</span>
+          </div>
+          <button type="submit" class="btn login-button" onClick={handleSubmit}>
+            Sign up
           </button>
-        </span>
-      </form>
+          <span className="mx-2">
+            Already a member?
+            <button className="link" onClick={handleClick}>
+              Log In!
+            </button>
+          </span>
+        </form>
+      </div>
     </div>
   );
 };
